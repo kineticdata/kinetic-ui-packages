280c47026642154722a02170b7491f2986bfc3d2
marcinzarycki <marcin.zarycki@kineticdata.com>
2020-10-07 16:47:32 -0500

    748946199, 702177654 Updated services package to use activity feed for list of requests

---
+++ src/components/shared/RequestActivity.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { connect } from '../../redux/store';
+import { compose, withHandlers, withProps } from 'recompose';
+import { RequestCard } from '../shared/RequestCard';
+import { ActivityFeed } from '@kineticdata/bundle-common';
+import { searchSubmissions, SubmissionSearch } from '@kineticdata/react';
+import * as constants from '../../constants';
+import { getSubmissionPath } from '../../utils';
+
+const emptyStateMessage = ({ type }) => {
+  switch (type) {
+    case 'Draft': {
+      return {
+        title: 'You have no draft requests.',
+        message:
+          "Draft services are forms you started but haven't submitted yet.",
+      };
+    }
+    case 'Open': {
+      return {
+        title: 'You have no open requests.',
+        message: 'If you request something, it will show up here.',
+      };
+    }
+    case 'Closed': {
+      return {
+        title: 'You have no closed requests.',
+        message:
+          "Closed requests are services you've requested that have been completed or canceled.",
+      };
+    }
+    default: {
+      return {
+        title: 'No requests found.',
+        message: 'Submit a service and it will show up here!',
+      };
+    }
+  }
+};
+
+const buildSearch = (coreState, username) => {
+  const searchBuilder = new SubmissionSearch()
+    .type(constants.SUBMISSION_FORM_TYPE)
+    .includes([
+      'details',
+      'values',
+      'form',
+      'form.attributes',
+      'form.kapp',
+      'form.kapp.attributes',
+    ]);
+
+  if (coreState === 'Draft') {
+    searchBuilder
+      .or()
+      .eq('createdBy', username)
+      .eq(`values[${constants.REQUESTED_BY_FIELD}]`, username)
+      .end();
+  } else {
+    searchBuilder
+      .or()
+      .eq(`values[${constants.REQUESTED_FOR_FIELD}]`, username)
+      .eq(`values[${constants.REQUESTED_BY_FIELD}]`, username)
+      .eq('submittedBy', username)
+      .eq('createdBy', username)
+      .end();
+  }
+  if (coreState) searchBuilder.coreState(coreState);
+
+  return searchBuilder.build();
+};
+
+const RequestActivityComponent = props => (
+  <ActivityFeed
+    feedKey={props.feedKey}
+    pageSize={props.pageSize || 10}
+    joinByDirection="DESC"
+    joinBy="createdAt"
+    options={{ type: props.type }}
+    dataSources={{
+      ...props.submissionsDataSource,
+    }}
+    contentProps={{
+      hidePaging: props.hidePaging,
+      emptyMessage: emptyStateMessage,
+    }}
+  />
+);
+
+export const RequestActivity = compose(
+  connect((state, props) => ({
+    kappSlug: state.app.kapp.slug,
+    username: state.app.profile.username,
+    appLocation: state.app.location,
+    coreState:
+      props.type === 'Draft'
+        ? constants.CORE_STATE_DRAFT
+        : props.type === 'Open'
+          ? constants.CORE_STATE_SUBMITTED
+          : props.type === 'Closed'
+            ? constants.CORE_STATE_CLOSED
+            : null,
+  })),
+  withHandlers({
+    buildRequestCard: props => record => (
+      <RequestCard
+        key={record.id}
+        submission={record}
+        path={getSubmissionPath(props.appLocation, record, null, props.type)}
+      />
+    ),
+  }),
+  withProps(props => ({
+    submissionsDataSource: {
+      requests: {
+        fn: searchSubmissions,
+        params: (prevParams, prevResult) =>
+          prevParams && prevResult
+            ? prevResult.nextPageToken
+              ? { ...prevParams, pageToken: prevResult.nextPageToken }
+              : null
+            : {
+                kapp: props.kappSlug,
+                limit: props.chunkSize || props.pageSize || 10,
+                search: buildSearch(props.coreState, props.username),
+              },
+        transform: result => ({
+          data: result.submissions,
+          nextPageToken: result.nextPageToken,
+        }),
+        component: props.buildRequestCard,
+      },
+    },
+  })),
+)(RequestActivityComponent);
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,7 +2,6 @@ import { reducer as app } from './modules/app';
 import servicesAppReducer from './modules/servicesApp';
 import formsReducer from './modules/forms';
 import searchReducer from './modules/search';
-import submissionsReducer from './modules/submissions';
 import submissionReducer from './modules/submission';
 import submissionCountsReducer from './modules/submissionCounts';
 import settingsFormsReducer from './modules/settingsForms';
@@ -13,7 +12,6 @@ export default {
   forms: formsReducer,
   search: searchReducer,
   servicesApp: servicesAppReducer,
-  submissions: submissionsReducer,
   submission: submissionReducer,
   submissionCounts: submissionCountsReducer,
   settingsForms: settingsFormsReducer,
