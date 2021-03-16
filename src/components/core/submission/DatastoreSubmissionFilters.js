b71b9e421df646fa61e34dbcf389a6d8ce292dbc
Matt Raykowski <matt.raykowski@gmail.com>
2021-03-16 09:27:57 -0500

    KP-2945: Changed the submission search to only allow searches to be built based upon the indexes available.

--- src/components/core/submission/DatastoreSubmissionFilters.js
+++ src/components/core/submission/DatastoreSubmissionFilters.js
@@ -1,15 +1,10 @@
 import { List, Map, Range } from 'immutable';
 import moment from 'moment';
 import { fetchForm, fetchKapp, fetchSpace } from '../../../apis';
-import {
-  defineKqlQuery,
-  INDEX_STATIC_PARTS,
-  TIMELINES,
-} from '../../../helpers';
+import { defineKqlQuery, TIMELINES } from '../../../helpers';
+import { availableParts, getUsedFields } from './helpers';
 
 export const MAX_PART_LENGTH = 10;
-
-const TIMELINES_AND_STATICS = TIMELINES.concat(INDEX_STATIC_PARTS).sort();
 
 export const filterDataSources = ({ formSlug, kappSlug }) => ({
   form: {
@@ -37,15 +32,8 @@ export const filterDataSources = ({ formSlug, kappSlug }) => ({
         .map(f => `values[${f.get('name')}]`),
     params: ({ form }) => form && [form],
   },
-  indexOptions: {
-    fn: form =>
-      form
-        .get('indexDefinitions')
-        .map(indexDefinition => ({
-          label: indexDefinition.get('name'),
-          value: indexDefinition.get('name'),
-        }))
-        .toArray(),
+  indexDefinitions: {
+    fn: form => form.get('indexDefinitions'),
     params: ({ form }) => form && [form],
   },
 });
@@ -78,15 +66,16 @@ const operatorChangeFn = i => ({ values }, { setValue }) => {
 };
 
 const partChangeFn = i => ({ values }, { setValue }) => {
-  const value = values.get(`op${i}-part`);
   // If the operator was set to something besides 'eq' or 'in' clear any
   // operators after this. Their change events will then fire and clear the
   // corresponding operands.
-  if (!value) {
-    values
-      .filter((value, name) => getPartIndex(name) > i)
-      .forEach((_value, name) => setValue(name, '', false));
-  }
+  values
+    .filter((value, name) => getPartIndex(name) > i)
+    .forEach((_value, name) => setValue(name, '', false));
+  setValue('range-part', '', false);
+  Range(0, MAX_PART_LENGTH).forEach(i =>
+    setValue(`orderby${i}-part`, '', false),
+  );
 };
 
 const orderChangeFn = i => ({ values }, { setValue }) => {
@@ -104,31 +93,110 @@ const enabledFn = i => ({ values }) =>
     .map(i => values.get(`op${i}-part`))
     .every(value => value);
 
-const visibleFn = (i, operatorType) => ({ values }) =>
-  operatorType
-    ? values.get(`op${i}-operator`) === operatorType
-    : i === 0 || values.get(`op${i - 1}-part`);
+const rangeVisibleFn = (operatorType, timeline = false) => ({
+  values,
+  indexDefinitions,
+}) => {
+  const usedFields = getUsedFields(values, -1, 'range');
+  const partsAvailable = availableParts(
+    values,
+    indexDefinitions,
+    usedFields,
+    'range',
+  );
 
-const availableOptions = currentPart => ({ values, fields }) => {
-  const usedFields = Range(0, MAX_PART_LENGTH)
-    .map(i => values.get(`op${i}-part`))
-    .toList()
-    .push(values.get('range-part'))
-    .filter(f => f !== '');
+  const isOperandValid = operatorType
+    ? !timeline
+      ? operatorType === 'between'
+        ? !TIMELINES.includes(values.get('range-part')) &&
+          values.get('range-operator') === 'between'
+        : !TIMELINES.includes(values.get('range-part'))
+      : operatorType === 'between'
+        ? TIMELINES.includes(values.get('range-part')) &&
+          values.get('range-operator') === 'between'
+        : TIMELINES.includes(values.get('range-part'))
+    : true;
 
-  return fields
-    .concat(
-      currentPart.startsWith('range')
-        ? TIMELINES_AND_STATICS
-        : INDEX_STATIC_PARTS,
-    )
-    .filter(
-      f =>
-        (currentPart.startsWith('range') && TIMELINES.includes(f)) ||
-        ((values.get(currentPart) === f || !usedFields.includes(f)) &&
-          !TIMELINES.includes(f)),
-    )
-    .map(f => Map({ label: f, value: f }));
+  return isOperandValid && partsAvailable.size > 0;
+};
+const visibleFn = (currentPart, i, operatorType) => ({
+  values,
+  indexDefinitions,
+}) => {
+  const hasRange = values.get('range-part') !== '';
+  const hasOrderBy =
+    Range(0, MAX_PART_LENGTH)
+      .map(i => values.get(`orderby${i}-part`))
+      .filter(v => v !== '')
+      .toList().size > 0;
+  const usedFields = getUsedFields(values, i, 'eq');
+  const partsAvailable = availableParts(
+    values,
+    indexDefinitions,
+    usedFields,
+    'eq',
+  );
+
+  // If this equality has this operator type selected.
+  if (operatorType) return values.get(`op${i}-operator`) === operatorType;
+
+  // If there's a range value set and this equality is not.
+  if ((hasRange || hasOrderBy) && !values.get(`op${i}-part`)) return false;
+
+  // If it is the first equality or the previous equality has a value set and
+  // this equality has available options.
+  return (i === 0 || values.get(`op${i - 1}-part`)) && partsAvailable.size > 0;
+};
+
+/* order by logic:
+ * 1. if no index is "fully used" then there are no options.
+ * 2. if an index is "fully used" then timelines are available.
+ * 3. if an index is "fully used" but the last part is used by a range, only that is available.
+ * 4. if an index is using all but the last element then that element is the only option and there is a timeline option
+ */
+
+/*
+ ({ values }) => {
+            const usedFields = Range(0, MAX_PART_LENGTH)
+              .map(i => values.get(`orderby${i}-part`))
+              .toList()
+              .filter(f => f !== '');
+
+            return (
+              fields
+                .concat(TIMELINES_AND_STATICS)
+                // Filter out the fields that are already used in parts.
+                .filter(
+                  f =>
+                    values.get(`orderby${i}-part`) === f ||
+                    !usedFields.includes(f),
+                )
+                .map(f => Map({ label: f, value: f }))
+            );
+          }
+ */
+
+const orderVisibleFn = partIndex => bindings => {
+  const { values } = bindings;
+
+  const prevPart = values.get(`orderby${partIndex - 1}-part`);
+  return (
+    partIndex === 0 ||
+    (!values.get('range-part') && prevPart && !TIMELINES.includes(prevPart))
+  );
+};
+
+const availableOptions = (partType, partIndex) => bindings => {
+  const { values, indexDefinitions } = bindings;
+
+  const usedFields = getUsedFields(values, partIndex, partType);
+  const partsAvailable = availableParts(
+    values,
+    indexDefinitions,
+    usedFields,
+    partType,
+  );
+  return partsAvailable.flatten().map(f => Map({ label: f, value: f }));
 };
 
 const getRValues = (operator, values, opBase, isTimeline = false) => {
@@ -216,25 +284,25 @@ const serializeQuery = ({ values }) => {
   };
 };
 
-export const filters = () => ({ form, fields, indexOptions }) =>
+export const filters = () => ({ form, indexDefinitions }) =>
   form &&
-  indexOptions && [
+  indexDefinitions && [
     ...Range(0, MAX_PART_LENGTH)
       .flatMap(i => [
         {
           enabled: enabledFn(i),
           name: `op${i}-part`,
           type: 'select',
-          visible: visibleFn(i),
+          visible: visibleFn(`op${i}-part`, i),
           onChange: partChangeFn(i),
-          options: availableOptions(`op${i}-part`),
+          options: availableOptions('eq', i),
           transient: true,
         },
         {
           enabled: enabledFn(i),
           name: `op${i}-operator`,
           type: 'select',
-          visible: visibleFn(i),
+          visible: visibleFn(`op${i}-part`, i),
           onChange: operatorChangeFn(i),
           options: [
             { label: '=', value: 'equals' },
@@ -249,28 +317,29 @@ export const filters = () => ({ form, fields, indexOptions }) =>
           name: `op${i}-operand1`,
           transient: true,
           type: 'text',
-          visible: visibleFn(i),
+          visible: visibleFn(`op${i}-part`, i),
         },
         {
           enabled: enabledFn(i),
           name: `op${i}-operand2`,
           transient: true,
           type: 'text',
-          visible: visibleFn(i, 'between'),
+          visible: visibleFn(`op${i}-part`, i, 'between'),
         },
         {
           enabled: enabledFn(i),
           name: `op${i}-operand3`,
           transient: true,
           type: 'text-multi',
-          visible: visibleFn(i, 'in'),
+          visible: visibleFn(`op${i}-part`, i, 'in'),
         },
       ])
       .toArray(),
     {
       name: 'range-part',
       type: 'select',
-      options: availableOptions('range-part'),
+      visible: rangeVisibleFn(),
+      options: availableOptions('range'),
       onChange: ({ values }, { setValue }) => {
         // Recalculate order by.
         const value = values.get('range-part');
@@ -286,6 +355,7 @@ export const filters = () => ({ form, fields, indexOptions }) =>
     {
       name: 'range-operator',
       type: 'select',
+      visible: rangeVisibleFn(),
       options: [
         { label: '>', value: 'greaterThan' },
         { label: '>=', value: 'greaterThanOrEquals' },
@@ -300,29 +370,25 @@ export const filters = () => ({ form, fields, indexOptions }) =>
       name: 'range-operand1',
       transient: true,
       type: 'text',
-      visible: ({ values }) => !TIMELINES.includes(values.get('range-part')),
+      visible: rangeVisibleFn('eq', false),
     },
     {
       name: 'range-operand2',
       transient: true,
       type: 'text',
-      visible: ({ values }) =>
-        !TIMELINES.includes(values.get('range-part')) &&
-        values.get('range-operator') === 'between',
+      visible: rangeVisibleFn('between', false),
     },
     {
       name: 'range-operand3',
       transient: true,
       type: 'datetime',
-      visible: ({ values }) => TIMELINES.includes(values.get('range-part')),
+      visible: rangeVisibleFn('eq', true),
     },
     {
       name: 'range-operand4',
       transient: true,
       type: 'datetime',
-      visible: ({ values }) =>
-        TIMELINES.includes(values.get('range-part')) &&
-        values.get('range-operator') === 'between',
+      visible: rangeVisibleFn('between', true),
     },
     {
       name: 'orderDirection',
@@ -342,33 +408,8 @@ export const filters = () => ({ form, fields, indexOptions }) =>
           type: 'select',
           onChange: orderChangeFn(i),
           enabled: ({ values }) => !values.get('range-part'),
-          options: ({ values }) => {
-            const usedFields = Range(0, MAX_PART_LENGTH)
-              .map(i => values.get(`orderby${i}-part`))
-              .toList()
-              .filter(f => f !== '');
-
-            return (
-              fields
-                .concat(TIMELINES_AND_STATICS)
-                // Filter out the fields that are already used in parts.
-                .filter(
-                  f =>
-                    values.get(`orderby${i}-part`) === f ||
-                    !usedFields.includes(f),
-                )
-                .map(f => Map({ label: f, value: f }))
-            );
-          },
-          visible: ({ values }) => {
-            const prevPart = values.get(`orderby${i - 1}-part`);
-            return (
-              i === 0 ||
-              (!values.get('range-part') &&
-                prevPart &&
-                !TIMELINES.includes(prevPart))
-            );
-          },
+          options: availableOptions('orderBy', i),
+          visible: orderVisibleFn(i),
         },
       ])
       .toArray(),
