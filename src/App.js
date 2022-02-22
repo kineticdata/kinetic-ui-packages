256030911a3d0610ed2a6c68d764243162965425
Matt Raykowski <matt.raykowski@gmail.com>
2022-02-22 13:47:13 -0600

    KP-5149: Added 'form type' column to forms list.

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Router } from '@reach/router';
+import { compose, lifecycle } from 'recompose';
+import { connect } from './redux/store';
+import { ErrorMessage, LoadingMessage } from '@kineticdata/bundle-common';
+import { I18n } from '@kineticdata/react';
+
+import { PageTitle } from './components/shared/PageTitle';
+import { Home } from './components/Home';
+import { Past } from './components/Past';
+import { TechBars } from './components/TechBars';
+import { Display } from './components/Display';
+import { Form } from './components/Form';
+import { AppointmentForm } from './components/AppointmentForm';
+import { Settings } from './components/settings/Settings';
+import { actions } from './redux/modules/techBarApp';
+
+const AppComponent = props => {
+  return props.render({
+    main: props.error ? (
+      <ErrorMessage
+        title="Unexpected Error"
+        message="Sorry, an unexpected error has occurred!"
+      />
+    ) : props.loading ? (
+      <LoadingMessage />
+    ) : (
+      <I18n>
+        <main className={`package-layout package-layout--tech-bar`}>
+          <PageTitle parts={['Loading...']} />
+          <Router>
+            <Settings path="settings/*" />
+            <Home path="/" />
+            <TechBars path="/tech-bars" />
+            <AppointmentForm path="/past/appointment/:techBarId/:id" />
+            <Past path="/past" />
+            <AppointmentForm path="/appointment/:techBarId/:id" />
+            <AppointmentForm path="/appointment/:techBarId" />
+            <AppointmentForm path="/appointment/:id" />
+            <AppointmentForm path="/appointment" />
+            <Form path="/forms/:formSlug/submissions/:id" />
+            <Form path="/forms/:formSlug" />
+            <Display path="/display/:techBarId/:mode" />
+            <Display path="/display/:techBarId" />
+          </Router>
+        </main>
+      </I18n>
+    ),
+  });
+};
+
+const mapStateToProps = (state, props) => ({
+  loading: state.techBarApp.loading,
+  error: state.techBarApp.error,
+  settingsBackPath: `/kapps/${state.app.kappSlug}`,
+});
+
+const mapDispatchToProps = {
+  fetchAppDataRequest: actions.fetchAppDataRequest,
+};
+
+const enhance = compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  lifecycle({
+    componentDidMount() {
+      this.props.fetchAppDataRequest();
+    },
+  }),
+);
+
+export const App = enhance(AppComponent);
