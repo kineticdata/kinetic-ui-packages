6bc932a920499e549b068a06f21dab031377e136
Nick <ndrafahl7@gmail.com>
2024-02-06 13:47:32 -0600

    KP:7263: Platform Bundle Epic (#461)
    * KP-7266: Platform bundle implementation (#414)
    
    * KP-7353 docker updates (#417)
    
    * new docker builds for the consoles
    
    * updated gha for new contexts & dockerfile locations
    * multistage build
    * moved dockerfile to docker subdir
    
    * updated Dockerfile paths
    
    * pinned apk packages
    
    * update context dir for snyk
    
    * try again to update context dir for snyk
    
    * attempt to use variable for context dir
    
    * add context-dir input var for snyk scan
    
    * remove extraneous yarn build from snyk image scan
    
    * set context dir in image builder
    
    * KP-7263 platform bundle docker build (#416)
    
    * Docker build for platform-bundle
    
    * moved dockerfile to conform to standard layout
    
    * added platform bundle jobs
    
    * pin platform apk versions
    
    * tell snyk to look for node_modules at the root
    
    * KP-7406 Added platform, hosted, and custom options
    
    * KP-7406 added Display page deprecation message and updated the name Host to Hosted
    
    * KP-7406 Updated the Display Page deprecation message.
    
    * KP-7406 made updates requested as per PR
    
    * KP-7516 React 18 and react router 6 upgrade in platform bundle
    
    * KP-7563: Fixes for GHA for Consoles After Multi-Stage Build Refactor (#446)
    
    * KP-7563: Add build-ci and test-ci scripts for platform-bundle.
    
    * KP-7563: Copy the nginx files from platform-bundle, not space-console.
    
    * KP-7563: Fix final image name, add scans to needs block for platform-bundle.
    
    * KP-7563: Bump kinetic-nginx to 1.0.8.
    
    * KP-7563: Cleanup Dockerfiles for Consoles; Remove build.sh script.
    
    * KP-7563: Remove no longer used Dockerfiles.
    
    * KP-7563: Cleanup local build.sh scripts for Consoles.
    
    * KP-7563: Refactor GHA workflows to drop usage of node on Host OS where we can.
    
    * KP-7563: Revert back to running yarn install to do node_modules scan.
    
    * KP-7700 Fixed platform bundle issue with router imports
    
    * KP-7706: Bump version of git installed via apk to 2.43.
    
    * KP-7707 Removed deprecated display types from space settings
    
    Hid fields that are no longer relevant in space and kapp forms.
    
    * KP-7732: 6.0.4 Bundle Deps and GHA Fixes (#460)
    
    * KP-7732: Bump deps to 6.0.4 for platform-bundle.
    
    * KP-7732: Scan shared node_modules, add flag to skip console dir.
    
    ---------
    
    Co-authored-by: marcinzarycki <marcin.zarycki@kineticdata.com>
    Co-authored-by: McClain Looney <mlooney@users.noreply.github.com>
    Co-authored-by: ChadRehmKineticData <chad.rehm@kineticdata.com>

--- src/components/core/kapp/KappForm.js
+++ src/components/core/kapp/KappForm.js
@@ -11,7 +11,7 @@ import {
 
 import { buildBindings, slugify } from '../../../helpers';
 
-const DISPLAY_TYPES = ['Display Page', 'Redirect'];
+const DISPLAY_TYPES = ['Redirect'];
 
 const SPACE_INCLUDES = 'details,spaceAttributeDefinitions';
 const KAPP_INCLUDES =
@@ -103,28 +103,28 @@ const fields = ({ kappSlug }) => ({ kapp }) =>
       label: 'After Logout Path',
       type: 'text',
       initialValue: get(kapp, 'afterLogoutPath'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'bundlePath',
       label: 'Bundle Path',
       type: 'text',
       initialValue: get(kapp, 'bundlePath'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'defaultFormConfirmationPage',
       label: 'Form Confirmation Page',
       type: 'text',
       initialValue: get(kapp, 'defaultFormConfirmationPage'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'defaultFormDisplayPage',
       label: 'Form Display Page',
       type: 'text',
       initialValue: get(kapp, 'defaultFormDisplayPage'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'defaultSubmissionLabelExpression',
@@ -151,8 +151,8 @@ const fields = ({ kappSlug }) => ({ kapp }) =>
         value: displayType,
         label: displayType,
       })),
-      enabled: ({ space }) => get(space, 'displayType') !== 'Single Page App',
-      initialValue: get(kapp, 'displayType') || 'Display Page',
+      visible: false,
+      initialValue: get(kapp, 'displayType'),
       helpText: 'Determines how the application works. Set in Space Settings.',
     },
     {
@@ -160,20 +160,20 @@ const fields = ({ kappSlug }) => ({ kapp }) =>
       label: ({ values }) =>
         values.get('displayType') === 'Redirect'
           ? 'Redirect URL'
-          : 'Kapp Display Page',
+          : 'Display Value',
       type: 'text',
       initialValue: get(kapp, 'displayValue'),
       required: ({ values }) => values.get('displayType') === 'Redirect',
       requiredMessage:
         "This field is required, when display type is 'Redirect'",
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'loginPage',
       label: 'Login Page',
       type: 'text',
       initialValue: get(kapp, 'loginPage'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'name',
@@ -193,7 +193,7 @@ const fields = ({ kappSlug }) => ({ kapp }) =>
       label: 'Reset Password Page',
       type: 'text',
       initialValue: get(kapp, 'resetPasswordPage'),
-      visible: ({ space }) => get(space, 'displayType') !== 'Single Page App',
+      visible: false,
     },
     {
       name: 'slug',
